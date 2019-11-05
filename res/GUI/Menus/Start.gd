b77f400d4bd2e2fa39586be0997e025921c730d9
Erisvan Junior <erisvan.junior.a@gmail.com>
2019-11-05 05:24:05 -0300

    feature: add menus
    - create styles from menus
    - create animations
    
    : please review the menus @Jaum

---
+++ res/GUI/Menus/Start.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func is_mouse_pressed(event: InputEvent) -> bool:
+	return event is InputEventMouseButton and event.is_pressed()
+
+func is_enter_pressed() -> bool:
+	return Input.is_action_pressed("ui_accept")
+
+func _input(event: InputEvent) -> void:
+	if is_mouse_pressed(event) or is_enter_pressed():
+		get_tree().change_scene('res://GUI/Menus/Main.tscn')
